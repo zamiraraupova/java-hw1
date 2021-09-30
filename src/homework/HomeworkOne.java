eeec4e1858474d748b8421854b36b599424ffab2
zamiraraupova <87194673+zamiraraupova@users.noreply.github.com>
2021-09-30 15:17:12 -0400

    initial commit

---
+++ src/homework/HomeworkOne.java
@@ -0,0 +1,85 @@
+package homework;
+//1.
+//public class HomeworkOne {
+//	public static void main(String[] args) {
+//	
+//		int x = 5;
+//	 
+//	 if (x>=90) {
+//		 System.out.println("It is A"); 
+//	 }
+//	 else if (x>=80 && x<89) {
+//		 System.out.println("It is B"); 
+//	 }
+//	 else if (x>=70 && x<79) {
+//		 System.out.println("It is C"); 
+//	 }
+//	 else if (x>=55 && x<69) {
+//		 System.out.println("It is D"); 
+//	 }
+//	 else {
+//		 System.out.println("It is F");
+//	 }
+//	}	 
+//}
+
+//2.
+//public class HomeworkOne {
+//	public static void main(String[] args) {
+//	
+//		int day = 8;
+//		
+//		switch (day) {
+//		  case 1:
+//		    System.out.println("Monday");
+//		    break;
+//		  case 2:
+//		    System.out.println("Tuesday");
+//		    break;
+//		  case 3:
+//		    System.out.println("Wednesday");
+//		    break;
+//		  case 4:
+//		    System.out.println("Thursday");
+//		    break;
+//		  case 5:
+//		    System.out.println("Friday");
+//		    break;
+//		  case 6:
+//		    System.out.println("Saturday");
+//		    break;
+//		  case 7:
+//		    System.out.println("Sunday");
+//		    break;
+//		  default:
+//			System.out.println("Invalid Input"); 	
+//		}
+//	}	 
+//}
+
+//3.
+//public class HomeworkOne {
+//	public static void main(String[] args) {
+//	
+//		int num = 17;
+//		
+//		if (num %2 != 0) {
+//			System.out.println("Cool"); }
+//		else if ( num %2 == 0) {
+//			if (num > 2 && num < 5) {
+//				System.out.println("Not Cool");
+//			}
+//		else if (num > 6 && num <= 20) {
+//				System.out.println("Cool");
+//			}
+//		else {
+//				System.out.println("Not Cool");
+//			}	
+//		}
+//		
+//	}	
+//	 
+//}
+
+
+
